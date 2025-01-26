4b8f4365ce9ba8639f5da87c919c158d88414e03
ryan-avancena <ravancena.dev@gmail.com>
2025-01-26 01:25:11 -0800

    preprocessing csvs

--- data.py
+++ data.py
@@ -1,75 +1,93 @@
 import pandas as pd
-import numpy as np
-import networkx as nx
-import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import StandardScaler
 
-""" 114000, {'track_id', 'artists', 'album_name', 'track_name',
-       'popularity', 'duration_ms', 'explicit', 'danceability', 'energy',
-       'key', 'loudness', 'mode', 'speechiness', 'acousticness',
-       'instrumentalness', 'liveness', 'valence', 'tempo', 'time_signature',
-       'track_genre'}"""
 tracks = pd.read_csv('../data/tracks.csv')
 
-""" (156422, {'spotify_id', 'name', 'followers', 'popularity', 'genres'})"""
-artists = pd.read_csv('../data/nodes.csv')
+identifiers = [
+    "track_id",
+    "artists",
+    "album_name",
+    "track_name",
+    "track_genre"
+]
 
-""" (300386, {'id_0', 'id_1'}) """ 
-edges = pd.read_csv('../data/edges.csv')
+theory_identifiers = [
+    "key",
+    "mode",
+    "tempo",
+    "time_signature"
+]
 
-def artist_graph():
-    top_10_artists = artists.sort_values(by='popularity', ascending=False)[:10]
-    # print(top_10_artists)
+numeric_features = [
+    "acousticness",
+    "popularity",
+    "danceability", 
+    "energy", 
+    "speechiness", 
+    "loudness", 
+    "speechiness",
+    "energy",
+    "instrumentalness",
+    "valence", 
+]
 
-    top_10_ids = set(top_10_artists['spotify_id']) 
+genre_groups = {
+    "acoustic": ["acoustic", "singer-songwriter", "songwriter", "guitar"],
+    "edm": ["house", "progressive-house", "techno", "trance", "electronic", "dubstep", "deep-house", "club", "detroit-techno", "minimal-techno"],
+    "rock": ["alt-rock", "alternative", "hard-rock", "punk-rock", "punk", "psych-rock", "grunge", "emo", "indie", "indie-pop", "j-rock"],
+    "pop": ["pop", "power-pop", "indie-pop", "k-pop", "mandopop", "j-pop", "cantopop"],
+    "hip-hop": ["hip-hop", "r-n-b"],
+    "classical": ["classical", "piano", "opera"],
+    "metal": ["black-metal", "death-metal", "heavy-metal", "metal", "metalcore", "hardcore", "grindcore"],
+    "folk": ["folk", "bluegrass"],
+    "jazz": ["jazz"],
+    "country": ["country", "honky-tonk"],
+    "latin": ["latin", "latino", "samba", "salsa", "pagode", "sertanejo", "mpb", "brazil"],
+    "reggae": ["reggae", "dub", "dancehall", "ska"],
+    "world": ["afrobeat", "turkish", "iranian", "malay", "world-music", "french", "german", "swedish", "spanish"],
+    "ambient": ["ambient", "chill", "study", "sleep", "new-age"],
+    "kids": ["children", "disney", "kids"],
+    "comedy": ["comedy", "party", "happy", "romance", "show-tunes"],
+    "other": ["anime", "j-dance", "j-idol", "british", "detroit-techno", "breakbeat"],
+}
 
-    # filter edges that involve the top 10 artists
-    filtered_edges = edges[(edges['id_0'].isin(top_10_ids)) | (edges['id_1'].isin(top_10_ids))]
-
-    # creating the graph
-    G = nx.Graph()
-    
-    # add nodes
-    for _, row in top_10_artists.iterrows():
-        G.add_node(row['spotify_id'], name=row['name'])  # Assuming 'name' is the column for artist names
-
-        # add edges
-        G.add_edges_from(filtered_edges[['id_0', 'id_1']].itertuples(index=False, name=None))
-
-        # plot the graph
-        plt.figure(figsize=(10, 8))
-        pos = nx.spring_layout(G)  # Positions for all nodes
-        nx.draw(G, pos, with_labels=True, node_size=700, node_color='skyblue', edge_color='gray', font_size=10)
-
-        # add labels to nodes (artist names)
-        labels = {row['spotify_id']: row['name'] for _, row in top_10_artists.iterrows()}
-        nx.draw_networkx_labels(G, pos, labels, font_size=12, font_color='black')
-
-        plt.title("Top 10 Artists Graph")
-        plt.show()
+def fix_artists(artist_list):
+    if isinstance(artist_list, str): 
+        artists = artist_list.split(';')  # split by semicolon
+        return ', '.join(artist.strip() for artist in artists)  
+    elif isinstance(artist_list, list):  # if it's already a list
+        return ', '.join(str(artist) for artist in artist_list)  # join them with commas
+    return ""
 
 
-def get_similar_songs(songlist):
-    numeric_features = [
-        "acousticness",
-        "popularity",
-        "danceability", 
-        "energy", 
-        "speechiness", 
-        "loudness", 
-        "speech",
-        "energy",
-        "instrumentalness",
-        "valence" 
+tracks["artists"] = tracks["artists"].apply(lambda v: fix_artists(v))
+tracks.insert(
+    0, "song_description", tracks["track_name"] + " - " + tracks["artists"]
+)
 
-        "key",
-        "mode",
-        "tempo",
-        "time_signature"
-    ]
+genre_to_category = {}
+for category, genres in genre_groups.items():
+    for genre in genres:
+        genre_to_category[genre] = category
 
-if __name__ == '__main__':
-    # top_10 = tracks.sort_values(by='popularity', ascending=False)[:10]
-    # get_similar_songs(top_10)
-    print(tracks.shape)
-    print(tracks.columns)
+tracks["genre_category"] = tracks["track_genre"].map(genre_to_category)
+tracks["genre_category"] = tracks["genre_category"].fillna("other")
+
+scaler = StandardScaler()
+scaled_features = scaler.fit_transform(tracks[numeric_features])
+scaled_tracks = pd.DataFrame(scaled_features, columns=numeric_features)
+
+scaled_tracks["track_id"] = tracks["track_id"]
+scaled_tracks["artists"] = tracks["artists"]
+scaled_tracks["track_name"] = tracks["track_name"]
+scaled_tracks["album_name"] = tracks["album_name"]
+scaled_tracks["track_genre"] = tracks["track_genre"]
+scaled_tracks["genre_category"] = tracks["genre_category"]
+scaled_tracks["song_description"] = tracks["song_description"]
+
+genre_tracks = {}
+for genre, group in genre_groups.items():
+    genre_tracks[genre] = scaled_tracks[scaled_tracks["track_genre"].isin(group)]
+
+print(genre_tracks['acoustic'].head())
